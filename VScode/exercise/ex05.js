e54025b7d7dbb61ab334a7eead727e23299e3c23
shusheng055 <tony051222@outlook.com>
2025-10-20 09:42:35 -0700

    1

--- VScode/exercise/ex05.js
+++ VScode/exercise/ex05.js
@@ -2,8 +2,13 @@ let count = 0;
 let colors = ["Orchid", "Coral", "Hotpink", "Plum"];
 
 $("#needy-button").click(function(){
-    $("#needy-button").html("Clicks: " + count + "Color: " + colors[count]);
+
+    let reminder = count % colors.length;
+
+    $("#needy-button").html("Clicks: " + count + " Color: " + colors[reminder]);
     //new line to add
+
+    $("body").css("background-color", colors[reminder]);
 
     count = count + 1;
 })

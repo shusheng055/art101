dad6fbcf830b8ba834740d59809dc13b3b5ddc7e
shusheng055 <tony051222@outlook.com>
2025-11-19 21:54:09 -0800

    1

--- VScode/garden/creature.js
+++ VScode/garden/creature.js
@@ -1,20 +1,30 @@
 $("#add-creature").click(
-    function(){
-        let crName = $("#crName").val();
-       
+    function () {
 
+        // grab the value from the input
+        let crName = $("#crName").val();
+        let crColor= $("#crColor").val();
+        let crEyesNum= $("#crEyesNum").val();
+        let crEyesHTML="";
+
+        for (let i = 0; i < crEyesNum ; i++){
+            crEyesHTML=crEyesHTML + "<div class=eye>.</div>";
+        }
+
+
+        // check for the field value do not add empty ones
+        if ( (crName == "") || (crName.length>12) ) { // do nothing 
+        }
+        else {
+            $("#creature-list").append(`
+<div class="creature">
+    <div class="creature-body" style="background: ${crColor}">${crEyesHTML}</div>
+    <div class="creature-info">${crName}</div>
+</div>
+`);
+        }
+
+        // remove the name after it's added
         $("#crName").val("");
 
-        if (crName == ""){
-            //do nothing
-            }
-
-        else if (crName.length > 12){
-            alert("Name Too Long")
-        }
-        
-        else { 
-            $("#creature-list").append(crName + "<br>");
-        }
-    }
-);
+    });
